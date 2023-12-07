87e2fcb3a7504088601f9a855949a7543ce29efc
Davi Guarda <davi.guarda.b@hotmail.com>
2023-12-07 14:11:35 -0300

    feat: Added spring security

---
+++ src/main/java/br/com/davidev/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.davidev.repositories;
+
+import br.com.davidev.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.userName = :userName")
+    User findByUsername(@Param("userName") String userName);
+}
